5d1b2ece28d9662e0a9ee84f722f7552e366f9e0
Roma <65kgm9ca@gmail.com>
2022-04-22 17:38:31 +0300

    client logger

---
+++ log/client_log_config.py
@@ -0,0 +1,43 @@
+"""
+Создание именованного логгера; +
+Сообщения лога должны иметь следующий формат: "<дата-время> <уровеньважности> <имямодуля> <сообщение>"; +
+Журналирование должно производиться в лог-файл; +
+На стороне сервера необходимо настроить ежедневную ротацию лог-файлов.
+"""
+import logging
+import os
+import sys
+
+
+# задаем форматирование сообщений
+CLIENT_FORMATTER = logging.Formatter('%(asctime)s %(levelname)s %(filename)s %(message)s')
+
+# выводим все в stderr
+STREAM_HANDLER = logging.StreamHandler(sys.stderr)
+# подключаем форматирование к обработчику
+STREAM_HANDLER.setFormatter(CLIENT_FORMATTER)
+# события не ниже ошибок (ERROR, CRITICAL)
+STREAM_HANDLER.setLevel(logging.ERROR)
+
+# путь до папки исполняемого файла
+LOG_PATH = os.path.dirname(os.path.abspath(__file__))
+# путь до файла логов
+LOG_PATH = os.path.join(LOG_PATH,
+                        'client_log.txt')
+LOG_FILE = logging.FileHandler(LOG_PATH,
+                               encoding='utf-8')
+LOG_FILE.setFormatter(CLIENT_FORMATTER)
+
+# создаем новый экземпляр, т.к. ранее такой логгер не определялся
+LOGGER = logging.getLogger('client')
+LOGGER.addHandler(STREAM_HANDLER)
+LOGGER.addHandler(LOG_FILE)
+LOGGER.setLevel(logging.ERROR)
+
+
+# отладка
+if __name__ == '__main__':
+    LOGGER.critical('Критическая ошибка')
+    LOGGER.error('Ошибка')
+    LOGGER.debug('Отладочная информация')
+    LOGGER.info('Информационное сообщение')
