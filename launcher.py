1853ca056c3d46c343485c93964be9a1d5a19904
Roma <65kgm9ca@gmail.com>
2022-04-30 18:56:12 +0300

    launcher

--- launcher.py
+++ launcher.py
@@ -1,5 +1,6 @@
-import subprocess
+"""Лаунчер"""
 
+import subprocess
 
 PROCESS = []
 
@@ -10,11 +11,14 @@ while True:
     if ACTION == 'q':
         break
     elif ACTION == 's':
-        PROCESS.append(subprocess.Popen(['python', 'server.py']))
+        PROCESS.append(subprocess.Popen('python server.py',
+                                        creationflags=subprocess.CREATE_NEW_CONSOLE))
         for i in range(2):
-            PROCESS.append(subprocess.Popen(['python', 'client.py', '-m send']))
+            PROCESS.append(subprocess.Popen('python client.py -m send',
+                                            creationflags=subprocess.CREATE_NEW_CONSOLE))
         for i in range(5):
-            PROCESS.append(subprocess.Popen(['python', 'client.py', '-m listen']))
+            PROCESS.append(subprocess.Popen('python client.py -m listen',
+                                            creationflags=subprocess.CREATE_NEW_CONSOLE))
     elif ACTION == 'x':
         while PROCESS:
             VICTIM = PROCESS.pop()
