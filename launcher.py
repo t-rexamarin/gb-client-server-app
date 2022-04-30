791df0e5ea40164d2e80171f56acb77ef5de4fa7
Roma <65kgm9ca@gmail.com>
2022-04-30 19:01:15 +0300

    without CREATE_NEW_CONSOLE

--- launcher.py
+++ launcher.py
@@ -11,14 +11,17 @@ while True:
     if ACTION == 'q':
         break
     elif ACTION == 's':
-        PROCESS.append(subprocess.Popen('python server.py',
-                                        creationflags=subprocess.CREATE_NEW_CONSOLE))
+        # PROCESS.append(subprocess.Popen('python server.py',
+        #                                 creationflags=subprocess.CREATE_NEW_CONSOLE))
+        PROCESS.append(subprocess.Popen(['python', 'server.py']))
         for i in range(2):
-            PROCESS.append(subprocess.Popen('python client.py -m send',
-                                            creationflags=subprocess.CREATE_NEW_CONSOLE))
+            # PROCESS.append(subprocess.Popen('python client.py -m send',
+            #                                 creationflags=subprocess.CREATE_NEW_CONSOLE))
+            PROCESS.append(subprocess.Popen(['python', 'client.py', '-m send']))
         for i in range(5):
-            PROCESS.append(subprocess.Popen('python client.py -m listen',
-                                            creationflags=subprocess.CREATE_NEW_CONSOLE))
+            # PROCESS.append(subprocess.Popen('python client.py -m listen',
+            #                                 creationflags=subprocess.CREATE_NEW_CONSOLE))
+            PROCESS.append(subprocess.Popen(['python', 'client.py', '-m listen']))
     elif ACTION == 'x':
         while PROCESS:
             VICTIM = PROCESS.pop()
