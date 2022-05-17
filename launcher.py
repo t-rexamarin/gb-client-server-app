5395c05692c6e2c0ea1ee4e8984244c6266c6dbc
Roma <65kgm9ca@gmail.com>
2022-05-17 20:46:29 +0300

    run multiple client from console

--- launcher.py
+++ launcher.py
@@ -1,5 +1,6 @@
 """Лаунчер"""
-
+import os
+import signal
 import subprocess
 from time import sleep
 
@@ -12,11 +13,29 @@ while True:
     if ACTION == 'q':
         break
     elif ACTION == 's':
-        PROCESS.append(subprocess.Popen('gnome-terminal -- python3 server.py', shell=True))
+        PROCESS.append(subprocess.Popen('gnome-terminal -- python3 server.py',
+                                        shell=True,
+                                        stdout=subprocess.PIPE,
+                                        preexec_fn=os.setsid))
         sleep(1)
-        for i in range(2):
-            PROCESS.append(subprocess.Popen(f'gnome-terminal -- python3 client.py -n Test{i}', shell=True))
+
+        while True:
+            CLIENTS = input('Сколько клиентов запустить?: ')
+            try:
+                CLIENTS = int(CLIENTS)
+            except ValueError:
+                print('Укажите числом кол-во клиентов.')
+            else:
+                break
+
+        for i in range(CLIENTS):
+            PROCESS.append(subprocess.Popen(f'gnome-terminal -- python3 client.py -n Test{i}',
+                                            shell=True,
+                                            stdout=subprocess.PIPE,
+                                            preexec_fn=os.setsid))
     elif ACTION == 'x':
         while PROCESS:
             VICTIM = PROCESS.pop()
             VICTIM.kill()
+            # VICTIM.terminate()
+            # os.killpg(os.getpgid(VICTIM.pid), signal.SIGKILL)
